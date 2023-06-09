adf6a6b038277d127e52fcd6652e045a9006c895
Lucas-Palma <palmalucassantiago@gmail.com>
2023-06-09 14:53:06 -0300

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,7 @@
+// GLOBALS
+
+//__dirname   - path to current directory
+// __filename - file name
+// requiere   - function to use modules (CommonJS)
+// module     - info about curren module (file)
+// process    - info about env where the program is being executed
---
+++ 3-modules.js
@@ -0,0 +1,12 @@
+// MODULES
+
+const { john, peter } = require('./4-names');
+const sayHi = require('./5-utiles');
+const data = require('./6-alternative');
+
+require('./7-mind-grenade')
+
+console.log(data)
+
+sayHi(john);
+sayHi(peter);
